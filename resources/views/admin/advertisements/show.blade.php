7e0e92c5a6dafa17c56a42daece723c6b6a4c510
krantiBaral <baralkranti4@gmail.com>
2024-08-01 22:13:40 +0545

    Update

--- resources/views/admin/advertisements/show.blade.php
+++ resources/views/admin/advertisements/show.blade.php
@@ -5,34 +5,34 @@
 @endpush
 
 @section('form_content')
-    <div class="row my-4">
-        <div class="col-md-7">
-            <div class="row form-group">
-                <div class="col-md-3">
-                    <label for=""><span class="show-text">Title:</span> </label><br>
-                </div>
-                <div class="col-md-8">
-                    {{ $item->title }}
-                </div>
+<div class="row my-4">
+    <div class="col-md-7">
+        <div class="row form-group">
+            <div class="col-md-3">
+                <label for=""><span class="show-text">Title:</span> </label><br>
             </div>
-
-            <div class="row form-group">
-                <div class="col-md-3">
-                    <label for=""><span class="show-text">Link:</span></label><br>
-                </div>
-                <div class="col-md-8">
-                    {!! $item->link ?? '---' !!}
-                </div>
-            </div>
-
-
-
-            <div class="col-md-4">
-                @if ($item->getImage())
-                    <img src="{{ $item->getImage() }}" alt="Image" width="50%">
-                @else
-                    <p>No image available</p>
-                @endif
+            <div class="col-md-8">
+                {{ $item->title }}
             </div>
         </div>
+
+        <div class="row form-group">
+            <div class="col-md-3">
+                <label for=""><span class="show-text">Link:</span></label><br>
+            </div>
+            <div class="col-md-8">
+                {!! $item->link ?? '---' !!}
+            </div>
+        </div>
+
+
+
+        <div class="col-md-4">
+            @if ($item->getImage())
+                <img src="{{ $item->getImage() }}" alt="Image" width="50%">
+            @else
+                <p>No image available</p>
+            @endif
+        </div>
+    </div>
     @endsection
