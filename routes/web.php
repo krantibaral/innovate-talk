1b0c1ae86574504fe017cf2d07e071a74ef90990
krantiBaral <baralkranti4@gmail.com>
2024-08-11 12:18:54 +0545

    Refinement

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::get('/', [FrontController::class, 'index'])->name('welcome');
 Route::get('/blog_details/{id}', [FrontController::class, 'blogDetails'])->name('blog_details');
 Route::post('/blog/{blogId}/comment', [FrontController::class, 'postComment'])->name('blog.comment');
 Route::get('/about', [FrontController::class, 'about'])->name('about');
+Route::get('/faq', [FrontController::class, 'faq'])->name('faq');
 Route::post('/contact-submit', [FrontController::class, 'sendEmail'])->name('contact-submit');
 
 Auth::routes();
